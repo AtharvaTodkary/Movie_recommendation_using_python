787c24ebd016581e41df7f3985cf205da520473a
Atharva Todkary <100767857+AtharvaTodkary@users.noreply.github.com>
2024-04-11 13:07:36 +0530

    Update Movie.py

--- application/Movie.py
+++ application/Movie.py
@@ -85,7 +85,7 @@ with st.form('recommendMovies'):
                     col1.image(movies[index][2], width=200)
                 except Exception as e:
                     col1.image("https://images.unsplash.com/photo-1509281373149-e957c6296406?q=80&w=1456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", width=200)
-                col1.markdown(f"<a href = '{movies[index][1]}'>Click</a>", True)
+                # col1.markdown(f"<a href = '{movies[index][1]}'>Click</a>", True)
 
                 # Increment index
                 index += 1
@@ -96,7 +96,7 @@ with st.form('recommendMovies'):
                     col2.image(movies[index][2], width=200)
                 except Exception as e:
                     col2.image("https://images.unsplash.com/photo-1509281373149-e957c6296406?q=80&w=1456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", width=200)
-                col2.markdown(f"<a href = '{movies[index][1]}'>Click</a>", True)
+                # col2.markdown(f"<a href = '{movies[index][1]}'>Click</a>", True)
 
                 # Increment index
                 index += 1
@@ -109,7 +109,7 @@ with st.form('recommendMovies'):
                 except Exception as e:
                     col3.image(
                         "https://images.unsplash.com/photo-1509281373149-e957c6296406?q=80&w=1456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", width=200)
-                col3.markdown(f"<a href = '{movies[index][1]}'>Click</a>", True)
+                # col3.markdown(f"<a href = '{movies[index][1]}'>Click</a>", True)
 
                 # Increment index
                 index += 1
